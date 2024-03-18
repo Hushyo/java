2d588081a671ac9952efca0ff76950a7fc76bc13
Hush <Hushsama@hotmail.com>
2024-03-18 19:09:44 +0800

    update

--- java-experiments/src/main/java/com/example/Test.java
+++ java-experiments/src/main/java/com/example/Test.java
@@ -5,6 +5,7 @@ import com.jogamp.opengl.FPSCounter;
 public class Test {
     public static void main(String[] args){
         test(1897,2010);
+        summation();
     }
     private static void test(int startYear, int endYear){
         for(int i = startYear; i <= endYear; i++){
@@ -13,4 +14,16 @@ public class Test {
             }
         }
     }
+    private static void summation(){
+        int repeat = 4;
+        double n = 3;
+        int resultNumber = 0;
+        int currentNumber = 0;
+        for( int i = 0; i<repeat; i++){
+            currentNumber += n*Math.pow(10,i);
+            resultNumber += currentNumber;
+        }
+        System.out.println(resultNumber);
+    }
+
 }
